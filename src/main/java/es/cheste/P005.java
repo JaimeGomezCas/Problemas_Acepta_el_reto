986c2d60aa25e6810235ee3037d854570ef4d234
alumno <jaimejgc2018@gmail.com>
2025-01-13 14:44:39 +0100

    p005 - proceso

--- src/main/java/es/cheste/P005.java
+++ src/main/java/es/cheste/P005.java
@@ -12,9 +12,11 @@ public class P005 {
             linea = sc.nextLine();
             lineas.add(linea);
         }
+
         casoDePrueba(pasarLineasAMattriz(lineas));
     }
     private static int[][] pasarLineasAMattriz(ArrayList<String> lineas){
+        lineas.remove(lineas.size() - 1);
         int[][] matriz = new int[lineas.size()][4];
 
         for (int i = 0; i < matriz.length; i++) {
@@ -31,9 +33,45 @@ public class P005 {
 
         return matriz;
     }
+    private static void imprimirMatriz(int[][] matriz){
+        for (int[] ints : matriz) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println();
+        }
+    }
     private static void casoDePrueba(int[][] matriz){
+        imprimirMatriz(matriz);
+        ArrayList<int[]> lineas = new ArrayList<>();
+        int contadorDeCeros = 1;
 
+        for (int i = 0; i < matriz.length; i++) {
+            contadorDeCeros--;
+            for (int j = 0; j < matriz[0].length; j++) {
+                if(matriz[i][j] == 0){
+                    contadorDeCeros ++;
+                }
+            }
+            lineas.add(matriz[i]);
+            if(contadorDeCeros == 0){
+                resolverCaso();
+                contadorDeCeros = 1;
+            }
+        }
 
 
     }
+    private static int[][] prepararMatriz(ArrayList<int[]> lineas){
+        int[][] matriz = new int[lineas.size()][4];
+        for (int i = 0; i < lineas.size(); i++) {
+            matriz[i] = lineas.get(0);
+            lineas.remove(0);
+        }
+        return matriz;
+    }
+    private static void resolverCaso(int[][] matriz){
+
+    }
+
 }
